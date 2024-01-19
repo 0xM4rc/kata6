1302a5c506b5c091137cafd12e421eaa360a445d
marc <marcos.sanchez2003@proton.me>
2024-01-19 16:25:48 +0000

    Passed test 2

--- src/main/java/software/ulpgc/kata6/Board.java
+++ src/main/java/software/ulpgc/kata6/Board.java
@@ -2,6 +2,8 @@ package software.ulpgc.kata6;
 
 public class Board {
     private final String[] state;
+    private final static char Alive = 'X';
+    private final static char Dead = '.';
 
     public Board(String[] state) {
         this.state = state;
@@ -12,7 +14,35 @@ public class Board {
     }
 
     public Board next() {
-        return new Board("");
+        return new Board(calculateNextState());
+    }
+
+    private String[] calculateNextState() {
+        String[] result = new String[rows()];
+        for (int i = 0; i < rows(); i++) {
+            result[i] = calculateNextState(i);
+        }
+        return result;
+    }
+
+    private String calculateNextState(int row) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < cols(); i++) {
+            result.append(calculateNextState(row, i));
+        }
+        return result.toString();
+    }
+
+    private char calculateNextState(int row, int col) {
+        return Dead;
+    }
+
+    private int cols() {
+        return state[0].length();
+    }
+
+    private int rows() {
+        return state.length;
     }
 
     public String state() {
--- src/test/java/tests/BoardTest.java
+++ src/test/java/tests/BoardTest.java
@@ -11,4 +11,11 @@ public class BoardTest {
         Board board = new Board("").next();
         assertThat(board.state()).isEqualTo("");
     }
+
+    @Test
+    public void should_return_1x1_board_with_all_dead_cells_given_1x1_board(){
+        Board board = new Board(".").next();
+        assertThat(board.state()).isEqualTo(".");
+    }
+
 }
