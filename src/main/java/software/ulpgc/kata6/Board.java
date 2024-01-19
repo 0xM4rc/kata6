23bd7aaccc4db77c82de3b2869736d8e07b87954
marc <marcos.sanchez2003@proton.me>
2024-01-19 17:11:57 +0000

    Modified Board && BoardTest

--- src/main/java/software/ulpgc/kata6/Board.java
+++ src/main/java/software/ulpgc/kata6/Board.java
@@ -1,5 +1,10 @@
 package software.ulpgc.kata6;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.stream;
+
 public class Board {
     private final String[] state;
     private final static char Alive = 'X';
@@ -34,7 +39,50 @@ public class Board {
     }
 
     private char calculateNextState(int row, int col) {
-        return Dead;
+        return anyRuleFor(row,col) ? Alive : Dead;
+    }
+
+    private boolean anyRuleFor(int row, int col) {
+        return rules.stream().anyMatch(rule -> rule.check(row, col));
+    }
+
+    private final List<Rule> rules = List.of(
+            (i, j) -> isAlive(i, j) && is(neighbors(i, j), 2, 3),
+            (i, j) -> isDead(i, j) && is(neighbors(i, j), 3)
+    );
+
+    private boolean isDead(int row, int col) {
+        return !isAlive(row, col);
+    }
+
+    private int neighbors(int row, int col) {
+        return (int) checksOf(row, col).stream()
+                .filter(Check::check)
+                .count();
+    }
+
+    private List<Check> checksOf(int row, int col) {
+        return List.of(
+                () -> isAlive(row -1, col -1),
+                () -> isAlive(row -1, col),
+                () -> isAlive(row, col +1),
+                () -> isAlive(row, col -1),
+                () -> isAlive(row +1, col -1),
+                () -> isAlive(row +1, col),
+                () -> isAlive(row+1, col +1),
+                () -> isAlive(row -1, col +1)
+        );
+    }
+
+    private boolean is(int expected, int... values){
+        return stream(values).anyMatch(value -> value == expected);
+    }
+    private boolean isAlive(int row, int col) {
+        return isInBound(row, col) && state[row].charAt(col) == Alive;
+    }
+
+    private boolean isInBound(int row, int col) {
+        return row >= 0 && row < rows() && col >= 0 && col < cols();
     }
 
     private int cols() {
@@ -47,5 +95,13 @@ public class Board {
 
     public String state() {
         return  String.join("\n",state);
+    }
+
+    private interface Rule {
+        boolean check(int row, int col);
+    }
+
+    private interface Check {
+        boolean check();
     }
 }
--- src/test/java/tests/BoardTest.java
+++ src/test/java/tests/BoardTest.java
@@ -18,4 +18,53 @@ public class BoardTest {
         assertThat(board.state()).isEqualTo(".");
     }
 
+    @Test
+    public void should_return_1x1_board_with_all_dead_cells_given_1x1_board_with_one_alive_cell(){
+        Board board = new Board("X").next();
+        assertThat(board.state()).isEqualTo(".");
+    }
+
+    @Test
+    public void should_return_2x2_board_with_all_alive_cells_given_2x2_board_with_all_alive_cells(){
+        Board board = new Board(Case.board2x2_with_all_alive_cells).next();
+        assertThat(board.state()).isEqualTo(Case.board2x2_with_all_alive_cells);
+    }
+
+    @Test
+    public void should_return_2x2_board_with_all_alive_cells_given_2x2_board_with_triangle_structure(){
+        Board board = new Board(Case.board2x2_with_triangle_structure).next();
+        assertThat(board.state()).isEqualTo(Case.board2x2_with_all_alive_cells);
+    }
+
+    @Test
+    public void should_return_2x2_board_with_all_dead_cells_given_2x2_board_with_strip_structure(){
+        Board board = new Board(Case.board2x2_with_strip_structure).next();
+        assertThat(board.state()).isEqualTo(Case.board2x2_with_all_dead_cells);
+    }
+
+
+
+
+    private class Case {
+        static final String board2x2_with_all_dead_cells = """
+                ..
+                ..
+                """.trim();
+
+        static final String board2x2_with_all_alive_cells = """
+                XX
+                XX
+                """.trim();
+
+        static final String board2x2_with_triangle_structure = """
+                .X
+                XX
+                """.trim();
+
+        static final String board2x2_with_strip_structure = """
+                ..
+                XX
+                """.trim();
+
+    }
 }
