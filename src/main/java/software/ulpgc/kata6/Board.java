3ff5b5b2f7feb145ee2921204776f17751aa8e44
marc <marcos.sanchez2003@proton.me>
2024-01-19 16:18:55 +0000

    1 test passed

--- src/main/java/software/ulpgc/kata6/Board.java
+++ src/main/java/software/ulpgc/kata6/Board.java
@@ -12,38 +12,10 @@ public class Board {
     }
 
     public Board next() {
-        return new Board(calculateNextState());
-    }
-
-    private String[] calculateNextState() {
-        String[] result = new String[rows()];
-        for (int i = 0; i < rows(); i++) {
-            result[i] = calculateNextState(i);
-        }
-        return result;
-    }
-
-    private String calculateNextState(int row) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < cols(); i++) {
-            result.append(calculateNextState(row, i));
-        }
-        return result.toString();
-    }
-
-    private char[] calculateNextState(int row, int i) {
-        return new char[]{'.'};
-    }
-
-    private int cols() {
-        return state[0].length();
-    }
-
-    private int rows() {
-        return state.length;
+        return new Board("");
     }
 
     public String state() {
-        return String.join("\n", state);
+        return  String.join("\n",state);
     }
 }
